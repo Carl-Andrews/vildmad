a7f16bdce9741bf21bc26f5277843d133fead7f7
Christian Moltke-Leth <chmo0002@stud.kea.dk>
2024-02-16 11:01:30 +0100

    javascript added

--- script.js
+++ script.js
@@ -1,2 +1,3 @@
 //https://eswqosjuxxhslqrooapo.supabase.co
 //eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVzd3Fvc2p1eHhoc2xxcm9vYXBvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc5MDEyMTAsImV4cCI6MjAyMzQ3NzIxMH0.gd6T8bVc5WRSI-CgOKj7B4vpnTFDECsZCBAVz90W3HM
+console.log("test");
