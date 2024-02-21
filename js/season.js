d9856fccfa341a1851f24b60f75242176600e200
Carl <carlandrews.k@gmail.com>
2024-02-21 12:07:25 +0100

    opdateret

--- js/season.js
+++ js/season.js
@@ -1,4 +1,4 @@
-fetch(`https://eswqosjuxxhslqrooapo.supabase.co/rest/v1/wild_food_gather/${categoi}.`, {
+fetch(`https://eswqosjuxxhslqrooapo.supabase.co/rest/v1/wild_food_gather/${categori}`, {
     method: "GET",
     headers:{
         apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVzd3Fvc2p1eHhoc2xxcm9vYXBvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc5MDEyMTAsImV4cCI6MjAyMzQ3NzIxMH0.gd6T8bVc5WRSI-CgOKj7B4vpnTFDECsZCBAVz90W3HM"
